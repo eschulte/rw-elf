7b3c40857069ce78fdc87d345664ced92e21f66e
Eric Schulte <schulte.eric@gmail.com>
2011-10-25 12:24:31 -0600

    successfully updating the .text section of an executable

--- extending-a-section.c
+++ extending-a-section.c
@@ -19,19 +19,14 @@ static char *extra_data = "goodbye world";
 int main(int argc, char *argv[])
 {
   int error, fd, result;
-  unsigned int flag;
-  struct stat sb;
   Elf *e;
   Elf_Scn *scn;
   Elf_Data *d;
-  const char *srcfile = argv[1];
-  const char *reffile = argv[2];
   off_t fsz1, fsz2;
-  char *tfn;
+  char *buf;
 
   e = NULL;
   fd = -1;
-  tfn = NULL;
 
   /* sanity checks */
   if ( argc != 2)
@@ -40,57 +35,42 @@ int main(int argc, char *argv[])
     errx(EX_SOFTWARE, " ELF library initialization failed : % s ",
          elf_errmsg(-1));
 
-  /* /\* Make a copy of the reference object. *\/ */
-  /* if ((tfn = elfts_copy_file(srcfile, &error)) < 0) { */
-  /*   printf("elfts_copyfile(%s) failed: \"%s\".", srcfile, strerror(error)); */
-  /* } */
-
   if((fd=open (argv[1], O_RDWR, 0)) < 0)
     err(EX_NOINPUT, " open \"% s \" failed ", argv[1]);
 
   if((e=elf_begin(fd, ELF_C_RDWR, NULL)) == NULL)
     errx(EX_SOFTWARE, "elf_begin() failed: %s.", elf_errmsg(-1));
 
-  if (stat(reffile, &sb) < 0) {
-    printf("fstat() failed: \"%s\".\n", strerror(errno));
-  }
-
   /* Retrieve section 1 and extend it. */
-  if ((scn = elf_getscn(e, 1)) == NULL) {
+  if ((scn = elf_getscn(e, 13)) == NULL) /* <- 13 indexes the .text section */
     printf("elf_getscn() failed: \"%s\".\n", elf_errmsg(-1));
-  }
 
-  if ((d = elf_newdata(scn)) == NULL) {
+  if ((d = elf_getdata(scn, NULL)) == NULL)
+    printf("getting data failed: \"%s\".\n", elf_errmsg(-1));
+
+  buf = (char *) d->d_buf;
+  printf("Old value of .text[471] was %x.\n", ((char *) d->d_buf)[471]);
+
+  if ((d = elf_newdata(scn)) == NULL)
     printf("elf_newdata() failed: \"%s\".\n", elf_errmsg(-1));
-  }
 
-  d->d_buf = extra_data;
-  d->d_size = strlen(extra_data);
+  /* <- change the last byte from nop to ret -- this has no runtime effect*/
+  d->d_buf = buf;
+  ((char *) d->d_buf)[471] = 195;
 
-  if (elf_flagscn(scn, ELF_C_SET, ELF_F_DIRTY) != ELF_F_DIRTY) {
+  printf("New value of .text[471] is  %x.\n", ((char *) d->d_buf)[471]);
+
+  /* flag the section as dirty */
+  if (elf_flagscn(scn, ELF_C_SET, ELF_F_DIRTY) != ELF_F_DIRTY)
     printf("elf_flagscn() failed: \"%s\".\n", elf_errmsg(-1));
-  }
 
-  if ((fsz1 = elf_update(e, ELF_C_NULL)) < 0) {
-    printf("elf_update(NULL) failed: \"%s\".\n", elf_errmsg(-1));
-  }
-
-  if (fsz1 != sb.st_size) {
-    printf("Size error: expected=%ld, elf_update()=%ld\n", sb.st_size, fsz1);
-  }
-
-  if ((fsz2 = elf_update(e, ELF_C_WRITE)) < 0) {
-    printf("elf_update(WRITE) failed: \"%s\".\n", elf_errmsg(-1));
-  }
-
-  if (fsz1 != fsz2) {
-    printf("fsz1 (%ld) != fsz2 (%ld)", fsz1, fsz2);
-  }
+  /* Sync the section */
+  if (elf_update(e, ELF_C_WRITE) < 0)
+    printf("elf_update() failed: \"%s\".", elf_errmsg(-1));
 
   /* Close the temporary file. */
-  if ((error = elf_end(e)) != 0) {
+  if ((error = elf_end(e)) != 0)
     printf("elf_end() failed: \"%s\".\n", elf_errmsg(-1));
-  }
 
   (void) close(fd);
   return 0;
