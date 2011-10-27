83b1459e3b91c9d4ec3eb0c4ae56ccb8c2a2de2d
Eric Schulte <schulte.eric@gmail.com>
2011-10-27 10:40:34 -0600

    macroified

--- elf.c
+++ elf.c
@@ -1,66 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int class;
 #define ELF32 1
 #define ELF64 2
 #define READ(buf,x,y) int_from_bytes(buf,(16 + (class * x)),(class * y))
+#define CLASS(buf)   buf[4]
+#define TYPE(buf)    READ(buf, 0,  1)
+#define MACHINE(buf) READ(buf, 1,  1)
+#define VERSION(buf) READ(buf, 2,  1)
+#define ENTRY(buf)   READ(buf, 4,  2)
+#define PHOFF(buf)   READ(buf, 8,  2)
+#define SHOFF(buf)   READ(buf, 12, 2)
 
+int class;
 char *read_raw(char *path);
-int int_from_bytes(char *buf, int pos, int num);
-
 void check_magic(char *buf);
 int elf_class(char *buf);
-int elf_type(char *buf); // 2
-int elf_machine(char *buf); // 2
-int elf_version(char *buf); // 4
-int entry(char *buf);
-int phoff(char *buf);
-int shoff(char *buf);
+int int_from_bytes(char *buf, int pos, int num);
 
-void main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
   char *buf = read_raw(argv[1]);
 
+  /* sanity check */
   check_magic(buf);
-  class = elf_class(buf);
+  class = CLASS(buf);
   switch (class){
-  case ELF32: printf("32-bit elf file.\n"); break;
-  case ELF64: printf("64-bit elf file.\n"); break;
-  default: printf("invalide elf class\n");
+  case ELF32: printf("class\t32-bit\n"); break;
+  case ELF64: printf("class\t64-bit\n"); break;
+  default: printf("invalid elf class\n"); return 1;
   }
 
-  printf("elf_type=%d\n", elf_type(buf));
-  printf("elf_machine=%d\n", elf_machine(buf));
-  printf("elf_version=%d\n", elf_version(buf));
-  printf("entry=%d\n", entry(buf));
-  printf("phoff=%d\n", phoff(buf));
-  printf("shoff=%d\n", shoff(buf));
-  printf("%d\n", READ(buf, 4, 2));
-  printf("%d\n", READ(buf, 4, 2));
+  /* print out some information about the elf file */
+  printf("type\t%d\n", TYPE(buf));
+  printf("machine\t%d\n", MACHINE(buf));
+  printf("version\t%d\n", VERSION(buf));
+  printf("entry\t%d\n", ENTRY(buf));
+  printf("phoff\t%d\n", PHOFF(buf));
+  printf("shoff\t%d\n", SHOFF(buf));
+
+  return 0;
 }
 
-int shoff(char *buf){
-  return READ(buf, 12, 2);
-}
-
-int phoff(char *buf){
-  return READ(buf, 8, 2);
-}
-
-int entry(char *buf){
-  return READ(buf, 4, 2);
-}
-
-int elf_version(char *buf){
-  return READ(buf, 2, 1);
-}
-
-int elf_machine(char *buf){
-  return READ(buf, 1, 1);
-}
-
-int elf_type(char *buf){
-  return READ(buf, 0, 1);
+int int_from_bytes(char *buf, int pos, int num){
+  char tmp[num];
+  int i, acc = 0;
+  for(i=0;i<num;i++) tmp[i] = buf[(pos + i)];
+  for(i=0;i<num;i++) acc += tmp[i] << (8 * i);
+  return acc;
 }
 
 int elf_class(char *buf){
@@ -73,14 +59,6 @@ void check_magic(char *buf){
   for(i=0; i<4; i++)
     if(!(buf[i] == magic[i]))
       printf("ELF Magic number test failed.");
-}
-
-int int_from_bytes(char *buf, int pos, int num){
-  char tmp[num];
-  int i, acc = 0;
-  for(i=0;i<num;i++) tmp[i] = buf[(pos + i)];
-  for(i=0;i<num;i++) acc += tmp[i] << (8 * i);
-  return acc;
 }
 
 char *read_raw(char *path){
