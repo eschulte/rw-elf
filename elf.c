ee35e64558ffc45d5bce16b3d190c66efaf491e6
Eric Schulte <eric.schulte@gmx.com>
2012-10-26 12:56:44 -0600

    split up elf.c into a library and an executable

--- elf.c
+++ elf.c
@@ -1,87 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "elf.h"
 
+/* Global State */
+int class;
+
+/* Elf Class */
 #define ELF32 1
 #define ELF64 2
-#define READ(buf,x,y) int_from_bytes(buf,(16 + (class * x)),(class * y))
-#define CREAD(buf,x,y) int_from_bytes(buf,(16 + (class * x)),y)
 
 /* Elf Header */
-#define CLASS(buf)   buf[4]
-#define TYPE(buf)    READ(buf, 0,  1)
-#define MACHINE(buf) READ(buf, 1,  1)
-#define VERSION(buf) READ(buf, 2,  1)
-#define ENTRY(buf)   READ(buf, 4,  2)
-#define PHOFF(buf)   READ(buf, 8,  2)
-#define SHOFF(buf)   READ(buf, 12, 2)
-#define FLAGS(buf)   CREAD(buf, 16, 2)
-#define EH_SZ(buf)   CREAD(buf, 18, 1)
-#define PH_E_SZ(buf) CREAD(buf, 19, 1)
-#define PH_NUM(buf)  CREAD(buf, 20, 1)
-#define SH_E_SZ(buf) CREAD(buf, 21, 1)
-#define SH_NUM(buf)  CREAD(buf, 22, 1)
-#define SH_SRND(buf) CREAD(buf, 23, 1)
+#define   CLASS(buf) buf[4]
+#define    TYPE(buf) int_from_bytes(buf, 16,                       2)
+#define MACHINE(buf) int_from_bytes(buf, 18,                       2)
+#define VERSION(buf) int_from_bytes(buf, 20,                       4)
+#define   ENTRY(buf) int_from_bytes(buf, 24,                       (class * 4))
+#define   PHOFF(buf) int_from_bytes(buf, 24 +       (class * 4),   (class * 4))
+#define   SHOFF(buf) int_from_bytes(buf, 24 +       (class * 8),   (class * 4))
+#define   FLAGS(buf) int_from_bytes(buf, 24 +       (class * 12),  4)
+#define   EH_SZ(buf) int_from_bytes(buf, 24 + (4  + (class * 12)), 2)
+#define PH_E_SZ(buf) int_from_bytes(buf, 24 + (6  + (class * 12)), 2)
+#define  PH_NUM(buf) int_from_bytes(buf, 24 + (8  + (class * 12)), 2)
+#define SH_E_SZ(buf) int_from_bytes(buf, 24 + (10 + (class * 12)), 2)
+#define  SH_NUM(buf) int_from_bytes(buf, 24 + (12 + (class * 12)), 2)
+#define SH_SRND(buf) int_from_bytes(buf, 24 + (14 + (class * 12)), 2)
 
 /* Section Header */
-#define SH_NAME(buf,shoff,shesz,ind) int_from_bytes(buf,(shoff + (shesz * ind)), (class * 2))
-#define SH_ADDR(buf,shoff,shesz,ind) int_from_bytes(buf,(shoff + (class * 8) + (shesz * ind)), (class * 4))
-#define SH_OFF(buf,shoff,shesz,ind) int_from_bytes(buf,(shoff + (class * 12) + (shesz * ind)), (class * 4))
-#define SH_SIZE(buf,shoff,shesz,ind) int_from_bytes(buf,(shoff + (class * 16) + (shesz * ind)), (class * 4))
+#define SH_NAME(buf,shoff,shesz,ind) int_from_bytes(buf, (shoff + (shesz * ind) + 0),                          4)
+#define SH_ADDR(buf,shoff,shesz,ind) int_from_bytes(buf, (shoff + (shesz * ind) + 12),               (class * 4))
+#define  SH_OFF(buf,shoff,shesz,ind) int_from_bytes(buf, (shoff + (shesz * ind) + 12 + (class * 4)), (class * 4))
+#define SH_SIZE(buf,shoff,shesz,ind) int_from_bytes(buf, (shoff + (shesz * ind) + 12 + (class * 8)), (class * 4))
 
-int class;
-void write_raw(char *path, char *buf, int size);
-int file_size(char *path);
-char *read_raw(char *path);
-void check_magic(char *buf);
-int elf_class(char *buf);
-unsigned int int_from_bytes(char *buf, int pos, int num);
-void print_header_info(char *buf);
-int text_section_header(char *buf);
-char *section_data(char *buf, int id);
-int get_text_address(char *path);
-int get_text_offset(char *path);
-int get_text_data_size(char *path);
-char *get_text_data(char *path);
-
-int main(int argc, char *argv[]){
-  if(argc != 3){
-    printf("usage: elf [input-file] [output-file]\n");
-    return 1;
-  }
-
-  char *in      = argv[1];
-  char *out     = argv[2];
-  char *buf     = read_raw(in);
-  long int size = file_size(in);
-  int offset    = get_text_offset(in);
-  int text_size = get_text_data_size(in);
-
-  /* update the last byte of the .text section */
-  buf[(offset + text_size - 1)] = 190;
-
-  write_raw(out, buf, size);
-
-  return 0;
+void sanity_check(char *buf){
+  check_magic(buf);
+  class = CLASS(buf);
+  if(!((class == ELF32) || (class == ELF64)))
+    printf("invalid elf class %d!\n", class);
 }
 
 int get_text_address(char *path){
   char *buf = read_raw(path);
   int text_shd;
 
-  /* sanity check */
-  check_magic(buf);
-  class = CLASS(buf);
-  if(!((class == 1) || (class == 2)))
-    printf("invalid elf class\n");
+  sanity_check(buf);
 
   /* find the text section */
-  if((text_shd = text_section_header(buf)) < 0)
-    printf("text sectin not found\n");
+  if((text_shd = text_section_header(buf)) < 0){
+    printf("text section not found\n");
+    return -1;
+  }
 
   /* get the size of the text section */
-  int shoff = SHOFF(buf);
-  int shesz = SH_E_SZ(buf);
-  int addr = SH_ADDR(buf,shoff,shesz,text_shd);
+  unsigned int shoff = SHOFF(buf);
+  unsigned int shesz = SH_E_SZ(buf);
+  unsigned int addr = SH_ADDR(buf,shoff,shesz,text_shd);
 
   free(buf);
   return addr;
@@ -91,15 +61,11 @@ int get_text_offset(char *path){
   char *buf = read_raw(path);
   int text_shd;
 
-  /* sanity check */
-  check_magic(buf);
-  class = CLASS(buf);
-  if(!((class == 1) || (class == 2)))
-    printf("invalid elf class\n");
+  sanity_check(buf);
 
   /* find the text section */
   if((text_shd = text_section_header(buf)) < 0)
-    printf("text sectin not found\n");
+    printf("text section not found\n");
 
   /* get the size of the text section */
   int shoff = SHOFF(buf);
@@ -114,15 +80,11 @@ int get_text_data_size(char *path){
   char *buf = read_raw(path);
   int text_shd;
 
-  /* sanity check */
-  check_magic(buf);
-  class = CLASS(buf);
-  if(!((class == 1) || (class == 2)))
-    printf("invalid elf class\n");
+  sanity_check(buf);
 
   /* find the text section */
   if((text_shd = text_section_header(buf)) < 0)
-    printf("text sectin not found\n");
+    printf("text section not found\n");
 
   /* get the size of the text section */
   int shoff = SHOFF(buf);
@@ -137,15 +99,11 @@ char *get_text_data(char *path){
   char *buf = read_raw(path);
   int text_shd;
 
-  /* sanity check */
-  check_magic(buf);
-  class = CLASS(buf);
-  if(!((class == 1) || (class == 2)))
-    printf("invalid elf class\n");
+  sanity_check(buf);
 
   /* find the text section */
   if((text_shd = text_section_header(buf)) < 0)
-    printf("text sectin not found\n");
+    printf("text section not found\n");
 
   /* get the .text data */
   unsigned char *text = section_data(buf, text_shd);
@@ -197,7 +155,7 @@ int text_section_header(char *buf){
     } while (name[j] != 0);
 
     /* show the section names */
-    /* printf("name[%d] %s\n", i, name); */
+    // printf("%d\t%s\n", i, name);
 
     if ((name[0] == '.') &&
         (name[1] == 't') &&
@@ -211,42 +169,49 @@ int text_section_header(char *buf){
   return -1;
 }
 
-void print_header_info(char *buf){
+int print_header_info(char *buf){
+  sanity_check(buf);
+
   switch (class){
   case ELF32: printf("class\t32-bit\n"); break;
   case ELF64: printf("class\t64-bit\n"); break;
   }
 
   /* print out some information about the elf file */
-  printf("type\t%d\n", TYPE(buf));
+  printf("type\t%d\n",    TYPE(buf));
   printf("machine\t%d\n", MACHINE(buf));
   printf("version\t%d\n", VERSION(buf));
-  printf("entry\t%d\n", ENTRY(buf));
-  printf("phoff\t%d\n", PHOFF(buf));
-  printf("shoff\t%d\n", SHOFF(buf));
-  printf("flags\t%d\n", FLAGS(buf));
-  printf("eh_sz\t%d\n", EH_SZ(buf));
+  printf("entry\t%d\n",   ENTRY(buf));
+  printf("phoff\t%d\n",   PHOFF(buf));
+  printf("shoff\t%d\n",   SHOFF(buf));
+  printf("flags\t%d\n",   FLAGS(buf));
+  printf("eh_sz\t%d\n",   EH_SZ(buf));
   printf("ph_e_sz\t%d\n", PH_E_SZ(buf));
-  printf("ph_num\t%d\n", PH_NUM(buf));
+  printf("ph_num\t%d\n",  PH_NUM(buf));
   printf("sh_e_sz\t%d\n", SH_E_SZ(buf));
-  printf("sh_num\t%d\n", SH_NUM(buf));
+  printf("sh_num\t%d\n",  SH_NUM(buf));
   printf("sh_srnd\t%d\n", SH_SRND(buf));
+  return 0;
 }
 
 unsigned int int_from_bytes(char *buf, int pos, int num){
   unsigned char tmp[num];
-  int i, t, acc = 0;
+  int i, t = 0;
+  unsigned int acc = 0;
   for(i=0;i<num;i++) tmp[i] = buf[(pos + i)];
   for(i=0;i<num;i++) acc += tmp[i] << (8 * i);
   return acc;
 }
 
-void check_magic(char *buf){
+int check_magic(char *buf){
   const char *magic = "ELF";
   int i;
   for(i=0; i<4; i++)
-    if(!(buf[i] == magic[i]))
+    if(!(buf[i] == magic[i])){
       printf("ELF Magic number test failed.");
+      return 1;
+    }
+  return 0;
 }
 
 char *read_raw(char *path){
@@ -256,11 +221,7 @@ char *read_raw(char *path){
 
   /* open file */
   file = fopen(path, "rb");
-  if (!file) {
-    fprintf(stderr, "Unable to open file %s\n", path);
-    return;
-  }
-	
+
   /* find file length */
   fseek(file, 0, SEEK_END);
   length=ftell(file);
