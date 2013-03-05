ae8b07d425090eb8e29e9ce926b5589b816c487c
Eric Schulte <schulte.eric@gmail.com>
2013-03-05 09:28:23 -0700

    cleaning up

--- elf-mutate.c
+++ elf-mutate.c
@@ -12,7 +12,8 @@
       text_offset=get_text_offset(in);          \
       text_size=get_text_data_size(in); } }
 
-unsigned char *buf;
+char *in = NULL;
+unsigned char *buf = NULL;
 int size, text_offset, text_size = 0;
 
 int text_ind(int offset){
@@ -22,57 +23,29 @@ int text_ind(int offset){
   return (offset+text_offset); }
 
 int main(int argc, char *argv[]){
-  int c;
-  char *in;
   char *help =
     "elf-mutate ELF [ACTIONS..]\n"
     "load ELF format FILE and perform ACTIONS\n"
     "\n"
     "Actions:\n"
     "  -h          print this help information\n"
-    "  -n          print the number of bytes\n"
-    /* Higher level methods
-    "  -d ID       replace instruction ID with nops\n"
-    "  -i ID1 ID2  copy instruction ID2 before instruction ID1\n"
-    "  -s ID1 ID2  swap instructions ID1 and ID2\n"
-    */
-    "  -G ID       get the value of byte ID\n"
-    "  -S ID NUM   set the value of byte ID to NUM\n"
+    "  -n          print the number of .text bytes\n"
+    "  -G ID       get the value of .text byte ID\n"
+    "  -S ID NUM   set the value of .text byte ID to NUM\n"
     "  -o OUTFILE  write current contents to OUTFILE";
+  int c;
   opterr = 0;
-  in = NULL;
-  buf = NULL;
-  size = text_offset = text_size = 0;
 
-  if(argc < 2){ puts(help); abort(); }
-  if(argv[1][0] == '-' && argv[1][1] == 'h'){ puts(help); return 1; }
+  if(argc < 2 || (argv[1][0] == '-' && argv[1][1] == 'h'))
+    puts(help); return 1;
   in = argv[1];
 
   if(access(in, F_OK) == -1){
-    printf("file `%s' not readable\n", in);
-    return 1; }
+    printf("File `%s' is not readable.\n", in); abort(); }
 
   while ((c = getopt (argc, argv, "hns:o:G:S:")) != -1)
     switch(c){
     case 'n': printf("text size %d\n", get_text_data_size(in)); break;
-      /*  Higher level methods
-    case 'd':
-      GETBUF;
-      stmt1 = atoi(optarg);
-      delete(buf, stmt1); break;
-    case 'i':
-      GETBUF;
-      stmt1 = atoi(optarg);
-      optind++;
-      stmt2 = atoi(argv[optind]);
-      insert(buf, stmt1, stmt2); break;
-    case 's':
-      GETBUF;
-      stmt1 = atoi(optarg);
-      optind++;
-      stmt2 = atoi(argv[optind]);
-      swap(buf, stmt1, stmt2); break;
-      */
     case 'G':
       GETBUF;
       printf("%x\n", buf[text_ind(atoi(optarg))]); break;
