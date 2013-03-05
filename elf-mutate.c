35ee5f2ea81eadf85c469d3207ce59127c98a317
Eric Schulte <schulte.eric@gmail.com>
2013-03-04 18:17:23 -0700

    fixed warning in elf-mutate

--- elf-mutate.c
+++ elf-mutate.c
@@ -1,21 +1,25 @@
 #include "elf.h"
 
 int main(int argc, char *argv[]){
+  char *in, *out, *buf;
+  long int size;
+  int offset, text_size;
+
   if(argc != 3){
     printf("usage: elf [input-file] [output-file]\n");
     return 1;
   }
 
-  char *in      = argv[1];
-  char *out     = argv[2];
-  char *buf     = read_raw(in);
-  long int size = file_size(in);
-  int offset    = get_text_offset(in);
-  int text_size = get_text_data_size(in);
-  
+  in        = argv[1];
+  out       = argv[2];
+  buf       = read_raw(in);
+  size      = file_size(in);
+  offset    = get_text_offset(in);
+  text_size = get_text_data_size(in);
+
   /* update the last byte of the .text section */
-  buf[(offset + text_size - 1)] = 190;
-  
+  buf[(offset + text_size - 1)] = (char)190;
+
   write_raw(out, buf, size);
 
   return 0;
