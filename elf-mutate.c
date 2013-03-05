5d6e514fafe2ac6d8f4a4c7005740e371c6d5507
Eric Schulte <schulte.eric@gmail.com>
2013-03-05 09:48:53 -0700

    updating documentation and unified reading

--- elf-mutate.c
+++ elf-mutate.c
@@ -30,28 +30,28 @@ int main(int argc, char *argv[]){
     "Actions:\n"
     "  -h          print this help information\n"
     "  -n          print the number of .text bytes\n"
-    "  -G ID       get the value of .text byte ID\n"
-    "  -S ID NUM   set the value of .text byte ID to NUM\n"
+    "  -s ID       get the value of .text byte ID\n"
+    "  -s ID NUM   set the value of .text byte ID to NUM\n"
     "  -o OUTFILE  write current contents to OUTFILE";
   int c;
   opterr = 0;
 
-  if(argc < 2 || (argv[1][0] == '-' && argv[1][1] == 'h'))
-    puts(help); return 1;
+  if(argc < 2 || (argv[1][0] == '-' && argv[1][1] == 'h')){
+    puts(help); return 1; }
   in = argv[1];
 
   if(access(in, F_OK) == -1){
     printf("File `%s' is not readable.\n", in); abort(); }
 
-  while ((c = getopt (argc, argv, "hns:o:G:S:")) != -1)
+  while ((c = getopt (argc, argv, "hng:s:o:")) != -1)
     switch(c){
-    case 'n': printf("text size %d\n", get_text_data_size(in)); break;
-    case 'G':
+    case 'n': printf("%d\n", get_text_data_size(in)); break;
+    case 'g':
       GETBUF;
       printf("%x\n", buf[text_ind(atoi(optarg))]); break;
-    case 'S':
+    case 's':
       GETBUF;
-      buf[text_ind(atoi(optarg))] = atoi(argv[optind]); break;
+      buf[text_ind(atoi(optarg))] = (unsigned int)strtol("90", 0, 16); break;
     case 'o':
       write_raw(optarg, buf, size);
       chmod(optarg, strtol("0777", 0, 8));
