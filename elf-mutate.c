a7d9e16787744f91de97e273cf2d2c70553a81ab
Eric Schulte <schulte.eric@gmail.com>
2013-03-05 08:10:49 -0700

    adding basic getter/setter output options

--- elf-mutate.c
+++ elf-mutate.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 void number(char *in){
   printf("text size %d\n", get_text_data_size(in)); }
@@ -19,7 +20,7 @@ void swap(char *buf, int stmt1, int stmt2){
   buf[stmt2] = tmp; }
 
 int main(int argc, char *argv[]){
-  int c, stmt1, stmt2;
+  int c, stmt1, stmt2, size;
   char *in, *buf;
   char *help =
     "elf-mutate ELF [ACTIONS..]\n"
@@ -31,9 +32,12 @@ int main(int argc, char *argv[]){
     "  -d ID       replace instruction ID with nops\n"
     "  -i ID1 ID2  copy instruction ID2 before instruction ID1\n"
     "  -s ID1 ID2  swap instructions ID1 and ID2\n"
+    "  -G ID       get the value of byte ID\n"
+    "  -S ID NUM   set the value of byte ID to NUM\n"
     "  -o OUTFILE  write current elf contents to OUTFILE";
   opterr = 0;
   buf = NULL;
+  size = 0;
 
   if(argc < 2){ puts(help); abort(); }
   if(argv[1][0] == '-' && argv[1][1] == 'h'){ puts(help); return 1; }
@@ -43,7 +47,7 @@ int main(int argc, char *argv[]){
     printf("file `%s' not readable\n", in);
     return 1; }
 
-  while ((c = getopt (argc, argv, "hnd:i:s:o:")) != -1)
+  while ((c = getopt (argc, argv, "hnd:i:s:o:G:S:")) != -1)
     switch(c){
     case 'n': number(in); break;
     case 'd':
@@ -51,6 +55,7 @@ int main(int argc, char *argv[]){
       optind++;
       stmt2 = atoi(argv[optind]);
       if(! buf){ buf = read_raw(in); }
+      size = file_size(in);
       delete(buf, stmt1); break;
     case 'i':
       stmt1 = atoi(optarg);
@@ -62,6 +67,21 @@ int main(int argc, char *argv[]){
       optind++;
       stmt2 = atoi(argv[optind]);
       swap(buf, stmt1, stmt2); break;
+    case 'G':
+      if(! buf){ buf = read_raw(in); }
+      size = file_size(in);
+      printf("%x\n", buf[atoi(optarg)]); break;
+    case 'S':
+      if(! buf){ buf = read_raw(in); }
+      size = file_size(in);
+      stmt1 = atoi(optarg);
+      optind++;
+      stmt2 = atoi(argv[optind]);
+      buf[stmt1] = stmt2; break;
+    case 'o':
+      write_raw(optarg, buf, size);
+      chmod(optarg, strtol("0777", 0, 8));
+      break;
     case 'h': puts(help); return 1;
     case '?':
       if (optopt == '1' || optopt == '2')
